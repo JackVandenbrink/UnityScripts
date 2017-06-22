53fd01854c79e79130c5cd560c60237c6df7b3d2
JackVandenbrink <jack.vandenbrink1997@gmail.com>
2017-06-22 19:01:03 +1000

    Begun folder structure

---
+++ camera/SimpleFreeCamFollow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFreeCamFollow: MonoBehaviour {
+
+	Camera cam;
+	public Transform target;
+	public float height;
+	public float maxDistance;
+
+	private float distance;
+	// Use this for initialization
+	void Start () {
+		
+		cam = GetComponent<Camera> ();
+		if (target == null) {
+			Debug.Log (this.name + ": No Target Set");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//TODO: Lerp to max distance from player
+		Vector3 camLoc = new Vector3(cam.transform.position.x,0,cam.transform.position.z);
+		Vector3 pawnLoc = new Vector3 (target.position.x, 0, target.position.z);
+
+		distance = Vector3.Distance(camLoc, pawnLoc);
+
+
+		if (distance > maxDistance) {
+			float distanceToTravel = distance - maxDistance;
+			Vector3 dir = camLoc - pawnLoc;
+
+			Vector3 destinationLoc = target.position +  (dir.normalized * maxDistance);
+			destinationLoc = new Vector3 (destinationLoc.x,height,destinationLoc.z);
+			cam.transform.position = destinationLoc;
+
+		}
+
+
+		cam.transform.LookAt(target);
+
+	}
+}
---
+++ pawn/MovePawn.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePawn : MonoBehaviour {
+
+	public GameObject pawn;
+	public Transform viewpoint;
+
+	public float speed = 1f;
+	public float jumpPower = 1f;
+	public float groundDist = 2f;
+
+
+
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		if (pawn == null) {
+			throw new UnityException ("pawn reference is null");
+
+		}
+		rb = pawn.GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ProcessMovement ();
+	}
+
+
+
+	void ProcessMovement() {
+		float forwardAxis = Input.GetAxisRaw ("MoveForward");
+		float strafeAxis = Input.GetAxisRaw ("MoveStrafe");
+		float up = Input.GetAxisRaw ("Jump");
+
+		Vector3 moveBy = new Vector3();
+
+		if (forwardAxis != 0) {
+			//moveBy += (Multiply(viewpoint.forward, Vector3.forward)) * forwardAxis;
+			moveBy += viewpoint.forward * forwardAxis;
+		}
+
+		if (strafeAxis != 0) {
+			//moveBy += (Multiply(viewpoint.right, Vector3.right)) * strafeAxis;
+			moveBy += viewpoint.right * strafeAxis;
+		}
+
+		moveBy.y = 0;
+		pawn.transform.Translate (moveBy.normalized * speed * Time.deltaTime);
+		if (up != 0) {			
+			if (IsGrounded () == true) {
+				Vector3 jump = new Vector3 (rb.velocity.x,jumpPower,rb.velocity.z);
+				rb.velocity = jump;	
+			}
+		}
+	}
+
+	bool IsGrounded() {
+		return Physics.Raycast (transform.position, -Vector3.up, groundDist);
+	}
+		
+
+}
